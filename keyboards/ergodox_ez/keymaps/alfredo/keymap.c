7a6682181c3de75ff57f9155a3d56247ede4df49
Alfredo Mapelli <alfredo.mapelli@dcslsoftware.com>
2018-03-02 23:06:58 +0000

    fix for like

--- keyboards/ergodox_ez/keymaps/alfredo/keymap.c
+++ keyboards/ergodox_ez/keymaps/alfredo/keymap.c
@@ -105,7 +105,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 		break;
     case AUTO_LIKE:
 		if (record->event.pressed) {
-			SEND_STRING("like '%%'"SS_TAP(KC_LEFT)SS_TAP(KC_LEFT));
+			SEND_STRING("like '%%'"SS_TAP(X_LEFT)SS_TAP(X_LEFT));
 		}
 		return false;
 		break;
